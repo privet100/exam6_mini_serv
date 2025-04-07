5841dbfe3536eff3a222fdb625d2f1b0d4dd09b5
akostrik <internet124816@gmail.com>
2025-04-07 20:06:26 +0200

    1

--- mini_serv.c
+++ mini_serv.c
@@ -10,11 +10,11 @@ typedef struct s_client {
     char    msg[290000];
 }   t_client;
 
-t_client    clis[1024];
-fd_set      read_set, write_set, cur_set;
-int         maxfd = 0, gid = 0;
-char        send_buffer[300000], recv_buffer[300000];
-socklen_t           len;
+char        buf_snd[300000], buf_rcv[300000];
+int         fdmax = 0, gid = 0;
+fd_set      setRd, setWr, setCr;
+socklen_t   len;
+t_client    clis[1024] = {0};
 
 void err(char  *msg) {
     write(2, msg, strlen(msg));
@@ -22,56 +22,53 @@ void err(char  *msg) {
 }
 
 void send_to_all(int sender) {
-    for (int fd = 0; fd <= maxfd; fd++)
-        if  (FD_ISSET(fd, &write_set) && fd != sender && send(fd, send_buffer, strlen(send_buffer), 0) == -1) err("Fatal error\n");
+    for (int fd = 0; fd <= fdmax; fd++)
+        if  (FD_ISSET(fd, &setWr) && fd != sender && send(fd, buf_snd, strlen(buf_snd), 0) == -1) err("Fatal error\n");
 }
 
 int main(int ac, char **av) {
     if (ac != 2) err("Wrong number of arguments\n");
+    int fdserv = socket(AF_INET, SOCK_STREAM, 0); if (fdserv == -1) err("Fatal error\n");
+    fdmax = fdserv;
+    FD_ZERO(&setCr);
+    FD_SET(fdserv, &setCr);
 
     struct sockaddr_in  serveraddr = {0};
-    int servfd = socket(AF_INET, SOCK_STREAM, 0); if (servfd == -1) err("Fatal error\n");
-    maxfd = servfd;
-
-    FD_ZERO(&cur_set);
-    FD_SET(servfd, &cur_set);
-    bzero(clis, sizeof(clis));
-
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
     serveraddr.sin_port = htons(atoi(av[1]));
 
-    if (bind(servfd, (const struct sockaddr *)&serveraddr, sizeof(serveraddr)) == -1 || listen(servfd, 100) == -1) err("Fatal error\n");
+    if (bind(fdserv, (const struct sockaddr *)&serveraddr, sizeof(serveraddr)) == -1 || listen(fdserv, 100) == -1) err("Fatal error\n");
 
     while (1) {
-        read_set = write_set = cur_set;
-        if (select(maxfd + 1, &read_set, &write_set, 0, 0) == -1) continue;
-        for (int fd = 0; fd <= maxfd; fd++) {
-            if (FD_ISSET(fd, &read_set)) {
-                if (fd == servfd) {
-                    int clifd = accept(servfd, (struct sockaddr *)&serveraddr, &len);
-                    if (clifd == -1) continue;
-                    if (clifd > maxfd) maxfd = clifd;
-                    clis[clifd].id = gid++;
-                    FD_SET(clifd, &cur_set);
-                    sprintf(send_buffer, "server: client %d just arrived\n", clis[clifd].id);
-                    send_to_all(clifd);
+        setRd = setWr = setCr;
+        if (select(fdmax + 1, &setRd, &setWr, 0, 0) == -1) continue;
+        for (int fd = 0; fd <= fdmax; fd++) {
+            if (FD_ISSET(fd, &setRd)) {
+                if (fd == fdserv) {
+                    int fdcli = accept(fdserv, (struct sockaddr *)&serveraddr, &len);
+                    if (fdcli == -1) continue;
+                    if (fdcli > fdmax) fdmax = fdcli;
+                    clis[fdcli].id = gid++;
+                    FD_SET(fdcli, &setCr);
+                    sprintf(buf_snd, "server: client %d just arrived\n", clis[fdcli].id);
+                    send_to_all(fdcli);
                 }
                 else {
-                    int ret = recv(fd, recv_buffer, sizeof(recv_buffer), 0);
+                    int ret = recv(fd, buf_rcv, sizeof(buf_rcv), 0);
                     if (ret <= 0) {
-                        sprintf(send_buffer, "server: client %d just left\n", clis[fd].id);
+                        sprintf(buf_snd, "server: client %d just left\n", clis[fd].id);
                         send_to_all(fd);
-                        FD_CLR(fd, &cur_set);
+                        FD_CLR(fd, &setCr);
                         close(fd);
                         bzero(clis[fd].msg, strlen(clis[fd].msg));
                     }
                     else
                         for (int i = 0, j = strlen(clis[fd].msg); i < ret; i++, j++) {
-                            clis[fd].msg[j] = recv_buffer[i];
+                            clis[fd].msg[j] = buf_rcv[i];
                             if (clis[fd].msg[j] == '\n') {
                                 clis[fd].msg[j] = '\0';
-                                sprintf(send_buffer, "client %d: %s\n", clis[fd].id, clis[fd].msg);
+                                sprintf(buf_snd, "client %d: %s\n", clis[fd].id, clis[fd].msg);
                                 send_to_all(fd);
                                 bzero(clis[fd].msg, strlen(clis[fd].msg));
                                 j = -1;
